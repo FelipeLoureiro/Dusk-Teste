d15430663f1acaf8b0d2cb5d5cd00916ee64175b
FelipeLoureiro <felipe.loureiro@before.com.br>
2017-06-16 10:17:45 -0400

    Factory

---
+++ tests/Browser/LoginPapTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginPapTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    public function testExample()
+    {
+        $user = factory(\App\User::class)->create([
+            'email' => '04782044186',
+            'password' => '20160810',
+
+        ]);
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('https://betasun.before.com.br/_sys/')
+            ->select('can_id', 2)
+            ->type("us_re", $user->email)
+            ->type("us_senha", $user->password)
+                ->press("Entrar");
+
+            $browser->pause(5000);
+        });
+    }
+}
